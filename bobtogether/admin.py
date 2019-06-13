eaaaba64f3fd5483349368e8079208f3aa3bde7c
chang02 <chang0022@snu.ac.kr>
2019-06-13 16:50:44 +0900

    add admin page

--- bobtogether/admin.py
+++ bobtogether/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description', 'menu', 'location']
+
+admin.site.register(Restaurant, RestaurantAdmin)
