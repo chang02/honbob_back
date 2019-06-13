7f5066e7c3e503a79547c13bc73167ce77d7bc55
chang02 <chang0022@snu.ac.kr>
2019-06-13 17:01:50 +0900

    admin site

--- bobtogether/admin.py
+++ bobtogether/admin.py
@@ -6,4 +6,20 @@ from .models import *
 class RestaurantAdmin(admin.ModelAdmin):
     list_display = ['name', 'description', 'menu', 'location']
 
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+class MatchingAdmin(admin.ModelAdmin):
+    list_display = ['owner', 'matchingMessage']
+
+class MatchingRequestAdmin(admin.ModelAdmin):
+    list_display = ['user', 'matching']
+
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ['user', 'message']
+
 admin.site.register(Restaurant, RestaurantAdmin)
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(Matching, MatchingAdmin)
+admin.site.register(MatchingRequest, MatchingRequestAdmin)
+admin.site.register(Notification, NotificationAdmin)
